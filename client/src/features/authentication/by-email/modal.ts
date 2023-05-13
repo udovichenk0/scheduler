24ffd3341e0a219f1c0a67a40527ffd069743189
Denis Udovichenko <denzeldenisq@gmail.com>
2023-05-13 21:06:49 +0300

    authenticaion/by-email validation

--- client/src/features/authentication/by-email/modal.ts
+++ client/src/features/authentication/by-email/modal.ts
@@ -1,13 +1,55 @@
 import { createEvent, createStore, sample } from "effector";
+import { createEffect } from "effector/effector.mjs";
+import { FormEvent } from "react";
+import { z } from 'zod'
 
 export const emailChanged = createEvent<string>()
 export const submitTriggered = createEvent()
-
 export const $email = createStore('')
-export const $emailError = createStore('')
+export const $emailError = createStore<'too_small' | 'invalid_string' |  null>(null)
+const emailSchema = z.string().email().min(4)
+export const onSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    submitTriggered()
+}
+
+const checkUserFx = createEffect(async(email:string) => {
+    console.log(email)
+    return email
+})
 
 sample({
     clock: emailChanged,
     target: $email
 })
+
+sample({
+    clock: submitTriggered,
+    source: $email,
+    filter: (email) => emailSchema.safeParse(email).success,
+    target: checkUserFx
+})
+
+sample({
+    clock: submitTriggered,
+    source: $email,
+    filter: (email) => !emailSchema.safeParse(email).success,    
+    fn: checkError,
+    target: $emailError
+})
+
+// const result = emailSchema.safeParse('')
+
+// if(!result.success){
+//     console.log(result.error.issues)
+// }
+
+function checkError(email:string){
+    if(email.length < 4){
+        return 'too_small'
+    }
+    else {
+        return 'invalid_string'
+    }
+}
 
--- client/src/features/authentication/by-email/ui.tsx
+++ client/src/features/authentication/by-email/ui.tsx
@@ -1,7 +1,7 @@
 import { useUnit } from "effector-react"
 import { Button } from "@/shared/ui/buttons/main-button"
 import { Input } from "@/shared/ui/input"
-import { $email, emailChanged } from "./modal"
+import { $email, emailChanged, onSubmit, submitTriggered } from "./modal"
 
 export const ByEmailForm = () => {
     const email = useUnit($email)
@@ -9,7 +9,7 @@ export const ByEmailForm = () => {
         <div>
             <h2 className="text-lg mb-3 font-semibold">Log in by email</h2>
             <p className="text-sm mb-7">Specify the address to log in to your account or register</p>
-            <form className="flex w-full flex-col gap-8">
+            <form className="flex w-full flex-col gap-8" onSubmit={(e) => onSubmit(e)}>
                 <Input onChange={emailChanged} value={email} label="Email" name=""/>
                 <span>
                     <Button intent={'blue-filled'} size={'medium'} title="Resume"/>
