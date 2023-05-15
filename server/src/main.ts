5d75c9609590f80101b5ce169d0aae300a6a3a44
Denis Udovichenko <denzeldenisq@gmail.com>
2023-05-15 21:27:45 +0300

    cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 import { PrismaService } from './database/prisma.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
   await app.listen(3000);
