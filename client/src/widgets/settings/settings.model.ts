9b9f32210d208f2588dc07e678286f2d9877feba
Denis Udovichenko <denzeldenisq@gmail.com>
2023-05-16 12:19:54 +0300

    settings move login to the modal

---
+++ client/src/widgets/settings/settings.model.ts
@@ -0,0 +1,12 @@
+import { createEvent, createStore, sample } from "effector";
+import { TabsEnum } from "./config";
+
+export const tabSelected = createEvent<TabsEnum>()
+
+export const $activeTab = createStore<TabsEnum>(TabsEnum.general)
+
+sample({
+    clock: tabSelected,
+    target: $activeTab
+})
+
--- client/src/widgets/settings/ui.tsx
+++ client/src/widgets/settings/ui.tsx
@@ -1,15 +1,12 @@
-import { useEffect, useState } from "react";
+import { useUnit } from "effector-react";
 
 import { Tab, TabPanel, Tabs } from "@/shared/ui/tab";
 import { SettingSvg } from "./assets/settings.svg";
 import { Synchronization } from "./assets/synchronization.svg";
+import { TabsEnum } from "./config";
+import { $activeTab, tabSelected } from "./settings.model";
 import { SynchronizationTab, GeneralTab } from './tabs'
 
-
-enum TabsEnum {
-	general,
-	synchronization
-}
 const tabs = [
 	{ Icon: SettingSvg, title: 'General', label: TabsEnum.general },
 	{ Icon: Synchronization, title: 'Synchronization', label: TabsEnum.synchronization}
@@ -18,21 +15,21 @@ const tabs = [
 const tabClassName = 'flex aria-[pressed=true]:text-white aria-[pressed=true]:fill-white fill-[#76899b] text-[#76899b] aria-[pressed=false]:hover:fill-white/60 aria-[pressed=false]:hover:text-white/60  flex-col gap-3 items-center'
 
 export const Settings = () => {
-	const [active, setActive] = useState<TabsEnum>(TabsEnum.general)
+	const activeTab = useUnit($activeTab)
 	return (
 		<div>
 			<div className="flex gap-5 border-b-[1px] border-white/10 px-6 pb-4">
-				<Tabs onChange={setActive}>
+				<Tabs onChange={tabSelected}>
 					{tabs.map(({Icon, title, label}) => 
-						<Tab label={label} aria-pressed={active == label} key={label} className={tabClassName}>
+						<Tab label={label} aria-pressed={activeTab == label} key={label} className={tabClassName}>
 							<Icon />
 							<span className="text-inherit">{title}</span>
 						</Tab>
 					)}
 				</Tabs>
 			</div>
-				<TabPanel activeValue={active} label={TabsEnum.general}><GeneralTab/></TabPanel>
-				<TabPanel activeValue={active} label={TabsEnum.synchronization}><SynchronizationTab/></TabPanel>
+				<TabPanel activeValue={activeTab} label={TabsEnum.general}><GeneralTab/></TabPanel>
+				<TabPanel activeValue={activeTab} label={TabsEnum.synchronization}><SynchronizationTab/></TabPanel>
 		</div>
 	)
 }
