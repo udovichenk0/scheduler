7a94151f0def1a9a01e1a2b2764af0f9dcb406a9
Denis Udovichenko <denzeldenisq@gmail.com>
2023-05-19 09:09:37 +0300

    session's set

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
 import { PrismaService } from './database/prisma.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:5173',
+    credentials: true,
   });
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
   await app.listen(3000);
