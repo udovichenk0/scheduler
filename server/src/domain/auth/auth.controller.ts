a83e6490ee11bfd2b47a3cb777bd0091edc5e343
Denis Udovichenko <denzeldenisq@gmail.com>
2023-05-21 12:17:14 +0300

    logout method

--- server/src/domain/auth/auth.controller.ts
+++ server/src/domain/auth/auth.controller.ts
@@ -3,26 +3,30 @@ import { AuthService } from './auth.service';
 import { AuthCredentialsDto, AuthDto } from './dto/auth.dto';
 import { Request } from 'express';
 @Controller('auth')
-@UsePipes(AuthCredentialsDto)
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('sign-up')
+  @UsePipes(AuthCredentialsDto)
   async Signup(
     @Body() body: AuthCredentialsDto,
-    @Req() req: Request,
     @Session() session: Record<string, any>,
   ) {
     const data = await this.authService.createUser(body);
-    session.user = data.refresh_token;
+    session.refresh_token = data.refresh_token;
     return AuthDto.create(data);
   }
   @Post('sign-in')
+  @UsePipes(AuthCredentialsDto)
   async Signin(
     @Body() body: AuthCredentialsDto,
     @Session() session: Record<string, any>,
   ) {
     const data = await this.authService.verifyUser(body);
-    session.user = data.refresh_token;
+    session.refresh_token = data.refresh_token;
     return AuthDto.create(data);
+  }
+  async Logout(@Session() session: Record<string, any>) {
+    session['refresh_token'] = null;
+    return;
   }
 }
