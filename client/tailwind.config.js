72108949e835a10c5a3d92bbde2fedb8a6c74c10
Denis Udovichenko <denzeldenisq@gmail.com>
2023-06-21 21:12:23 +0300

    color variables added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: [
-    "class"
-  ],
+  darkMode: 'class',
+  lightMode: 'class',
+  defaultMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -13,7 +13,19 @@ export default {
         'main-blue': '#0b0417',
         'azure': '#2ebcff',
         'error': '#ff5f58',
-        'grey': '#76899b'
+        'grey': '#76899b',
+        'space': '#76899b',
+        'main': "var(--main)",
+        'accent': "var(--accent)",
+        'primary': "var(--primary)",
+        'cHover': 'var(--cHover)',
+        'brand': 'var(--brand)',
+        'cFont': 'var(--cFont)',
+        'cBorder': 'var(--cBorder)',
+        'cFocus': 'var(--cFocus)',
+        'cOpacitySecondFont': 'var(--cOpacitySecondFont)',
+        'cSecondBorder': 'var(--cSecondBorder)',
+        'cLeftBottomPanel': 'var(--cLeftBottomPanel)'
       }
     },
   },
