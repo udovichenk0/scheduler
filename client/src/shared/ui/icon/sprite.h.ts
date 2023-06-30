5f7ffbd7a0f7630b33f9d279c04db0b81b006b3c
Denis Udovichenko <denzeldenisq@gmail.com>
2023-06-30 21:21:26 +0300

    spaces

--- client/src/shared/ui/icon/sprite.h.ts
+++ client/src/shared/ui/icon/sprite.h.ts
@@ -1,10 +1,7 @@
-
 export interface SpritesMap {
-    'common': "arrow"|"calendar"|"cloud"|"done"|"inbox"|"mail"|"palette"|"plus"|"settings"|"star"|"upcoming";
-'eye': "close"|"open";
-  }
-
+  'common': "arrow"|"calendar"|"cloud"|"done"|"inbox"|"mail"|"palette"|"plus"|"settings"|"star"|"upcoming";
+  'eye': "close"|"open";
+}
 export const SPRITES_META: { [K in keyof SpritesMap]: SpritesMap[K][]; } = {
   'common': ["arrow","calendar","cloud","done","inbox","mail","palette","plus","settings","star","upcoming"],'eye': ["close","open"]
 };
-  
