6f0bad89be15b570fac8f18406ffae030067e447
Denis Udovichenko <denzeldenisq@gmail.com>
2023-07-03 11:42:31 +0300

    change accent colors functionality

--- client/src/widgets/settings/tabs/theme/theme.model.ts
+++ client/src/widgets/settings/tabs/theme/theme.model.ts
@@ -2,29 +2,47 @@ import { createEvent, createStore, sample, createEffect } from "effector";
 import { createGate } from 'effector-react'
 import Cookies from 'universal-cookie';
 type Theme = 'space' | 'default' | 'dark' | 'light' | 'grey'
+type Accent = 'blue' | 'yellow' | 'green' | 'red' | 'orange' | 'purple' | 'pink'
 export const themeChanged = createEvent<Theme>()
+export const accentChanged = createEvent<Accent>()
 export const cookies = new Cookies()
 
 export const $theme = createStore<Theme | null>(null)
+export const $accent = createStore<Accent | null>(null)
+
+export const themeGate = createGate() 
 
 const changeThemeFx = createEffect((theme: Theme) => {
   cookies.set('theme', theme)
   document.documentElement.setAttribute('data-theme', theme)
 })
+
+//TODO everything move to entity probly
+const changeAccentFx = createEffect((accent: Accent) => {
+  cookies.set('accent', accent)
+  document.documentElement.style.setProperty('--accent', `var(--${accent})`)
+})
+
 const getThemeFx = createEffect(() => {
   const theme = cookies.get('theme') as Theme || 'space'
   return theme
 })
-
-export const themeGate = createGate() 
+const getAccentFx = createEffect(() => {
+  const accent = cookies.get('accent') as Accent || 'blue'
+  return accent
+})
 
 sample({
   clock: themeGate.open,
-  target: getThemeFx
+  target: [getThemeFx, getAccentFx]
 })
 sample({
   clock: getThemeFx.doneData,
   target: $theme
+})
+sample({
+  clock: getAccentFx.doneData,
+  target: $accent
 })
 
 sample({
@@ -33,9 +51,19 @@ sample({
 })
 
 sample({
+  clock: accentChanged,
+  target: changeAccentFx 
+})
+
+sample({
   clock: changeThemeFx.done,
   fn: ({params}) => params,
   target: $theme
+})
+sample({
+  clock: changeAccentFx.done,
+  fn: ({params}) => params,
+  target: $accent
 })
 // use effector-storage
 
