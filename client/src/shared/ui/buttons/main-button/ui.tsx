1105930af948f247d2cdadf9864259e6bd437f70
Denis Udovichenko <denzeldenisq@gmail.com>
2023-07-07 23:12:33 +0300

    show-date-title label, right slot for Button

--- client/src/shared/ui/buttons/main-button/ui.tsx
+++ client/src/shared/ui/buttons/main-button/ui.tsx
@@ -6,17 +6,18 @@ import { buttonCva } from "./cva.styles";
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonCva> {
     icon?: ReactNode,
+    rightSlot?: ReactNode
     title?: string,
 }
 
 
-export const Button = ({ intent, size, icon, title, ...props }: ButtonProps) => {
+export const Button = ({ intent, size, icon, title,rightSlot, ...props }: ButtonProps) => {
   const { className, ...rest } = props
   return (
     <>
       <button className={clsx(buttonCva({ size, intent }), className)} {...rest}>
         <div className='flex gap-4 items-center jusfity-center'>
-          {!!icon && icon } {title}
+          {!!icon && icon } <div className='flex items-center gap-2'>{title} {rightSlot}</div>
         </div>
       </button>
     </>
