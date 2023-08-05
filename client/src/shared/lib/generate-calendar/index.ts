61c99ded946f9a7d656c9a0c4eb7e1ad3ebb756d
Denis Udovichenko <denzeldenisq@gmail.com>
2023-08-06 00:02:23 +0300

    change from new Array to Array.from

--- client/src/shared/lib/generate-calendar/index.ts
+++ client/src/shared/lib/generate-calendar/index.ts
@@ -4,8 +4,8 @@ export const generateCalendar = (currentMonth = dayjs().month()) => {
   const year = dayjs().year()
   const firstDayOfMonth = dayjs(new Date(year, currentMonth - 1)).day()
   let current = 0 - firstDayOfMonth
-  return new Array(5).fill([]).map(() => {
-    return new Array(7).fill(null).map(() => {
+  return Array.from({ length: 5 }, () => []).map(() => {
+    return Array.from({ length: 7 }, () => null).map(() => {
       current += 1
       const currentDate = dayjs(new Date(year, currentMonth, current + 8))
       return {
