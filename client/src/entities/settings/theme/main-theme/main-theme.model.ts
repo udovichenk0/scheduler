4b244c63ce995963d3b127ce2d69e1a9c3b358ef
Denis Udovichenko <denzel2.denis@gmail.com>
2023-08-07 20:33:42 +0300

    fix(move cell to separate component)

--- client/src/entities/settings/theme/main-theme/main-theme.model.ts
+++ client/src/entities/settings/theme/main-theme/main-theme.model.ts
@@ -3,16 +3,17 @@ import { createEffect } from "effector/effector.umd"
 import { z } from "zod"
 
 import { cookiePersist } from "@/shared/lib/cookie-persist"
-//TODO change naming to ThemeContract
-const ThemeSchema = z.enum(["space", "default", "dark", "light", "grey"])
+const ThemeContract = z.enum(["space", "default", "dark", "light", "grey"])
 
-export type Theme = z.infer<typeof ThemeSchema>
+export type Theme = z.infer<typeof ThemeContract>
 export const themeChanged = createEvent<Theme>()
 
-export const $theme = createStore<Theme>("default")
+export const $theme = createStore<Theme>('default')
 
 const changeDateThemeFx = createEffect((theme: Theme) => {
-  document.documentElement.setAttribute("data-theme", theme)
+  if(theme){
+    document.documentElement.setAttribute("data-theme", theme)
+  }
 })
 sample({
   clock: themeChanged,
--- client/src/shared/theme/create-theme.ts
+++ client/src/shared/theme/create-theme.ts
@@ -6,7 +6,10 @@ export const createTheme = () => {
   const init = createEvent()
   const setMainThemeFx = createEffect(() => {
     const theme = parseCookieValue("theme")
-    document.documentElement.setAttribute("data-theme", `${theme || "default"}`)
+    if(theme) {
+      console.log(theme)
+      document.documentElement.setAttribute("data-theme", `${theme}`)
+    }
   })
   const setAccentThemeFx = createEffect(() => {
     const accent = parseCookieValue("accent")
