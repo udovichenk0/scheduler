c6ed2940f8eb1f2c01fbe9515844938dc0e64cdc
Denis Udovichenko <denzeldenisq@gmail.com>
2023-08-15 16:17:58 +0300

    feature(delete unverified users after 24 hours)

--- server/src/nodemailer/send-email.ts
+++ server/src/nodemailer/send-email.ts
@@ -3,7 +3,7 @@ import { transporter } from '.';
 export const sendEmail = async (email: string, token: string) => {
   const info = await transporter.sendMail(
     {
-      from: 'denzel2.denis@gmail.com',
+      from: process.env.EMAIL,
       to: email,
       subject: 'Verify your email',
       html: `<h1>${token}</h1>`,
@@ -17,7 +17,3 @@ export const sendEmail = async (email: string, token: string) => {
   );
   return info;
 };
-//!Another user trying to signup with the sasme email that hasn't been verified by first user
-
-//!If first user didn't verify an account, and come back later and try to signup/signin again with the same email
-//TODO if he signin we should check if the email is verified and if not send an code to email
