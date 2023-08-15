05cfd0f8c2bc991fc9b7995f6781e2191a3869e4
Denis Udovichenko <denzeldenisq@gmail.com>
2023-08-15 16:17:58 +0300

    feature(delete unverified users after 24 hours)

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,28 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
 import { ConfigService } from '@nestjs/config';
-
+import { job } from 'cron';
+import { PrismaClient } from '@prisma/client';
+const prismaClient = new PrismaClient();
+job(
+  '0 */24 * * *',
+  async function () {
+    await prismaClient.user.deleteMany({
+      where: {
+        AND: [
+          { verified: false },
+          {
+            created_at: {
+              lte: new Date(new Date().getTime() - 24 * 60 * 60 * 1000),
+            },
+          },
+        ],
+      },
+    });
+  },
+  null,
+  true,
+);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
