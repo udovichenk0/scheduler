9a0eda01ef0c79051d0382eaf04ee795173d2897
Denis Udovichenko <denzeldenisq@gmail.com>
2023-08-15 20:51:15 +0300

    fix(made a template for similar auth forms)

--- client/src/features/authentication/sign-up/ui.tsx
+++ client/src/features/authentication/sign-up/ui.tsx
@@ -1,10 +1,9 @@
 import { useUnit } from "effector-react"
-import { FormEvent, useRef, useState } from "react"
+import { useRef, useState } from "react"
 
 import { Button } from "@/shared/ui/buttons/main-button"
-import { Typography } from "@/shared/ui/general/typography"
-import { Icon } from "@/shared/ui/icon"
 import { Input } from "@/shared/ui/data-entry/main-input"
+import { AuthTemplate } from "@/shared/ui/templates/auth/ui"
 
 import { $email } from "../by-email"
 import { TogglePasswordButton } from "../shared/ui/toggle-password-button"
@@ -15,12 +14,8 @@ import {
   passwordChanged,
   submitTriggered,
 } from "./signup.modal"
-const onSubmit = (e: FormEvent, submit: () => void) => {
-  e.preventDefault()
-  submit()
-}
 export const Signup = ({ goBack }: { goBack: () => void }) => {
-  const [password, error, email, changePassword, submit] = useUnit([
+  const [password, error, email, changePassword, onSubmit] = useUnit([
     $password,
     $passwordError,
     $email,
@@ -30,55 +25,38 @@ export const Signup = ({ goBack }: { goBack: () => void }) => {
   const [isPasswordShown, togglePasswortView] = useState(false)
   const ref = useRef<HTMLInputElement>(null)
   return (
-    <div className="relative text-center text-cFont">
-      <Button
-        className="absolute -left-[20px]"
-        onClick={goBack}
-        intent={"primary"}
-        size={"sm"}
-      >
-        <Icon
-          name="common/arrow"
-          className="text-4 rotate-180 text-cIconDefault"
-        />
-      </Button>
-      <Typography.Heading size="base" className="mb-3 font-medium">
-        Registration
-      </Typography.Heading>
-      <Typography.Paragraph size="sm" className="mb-7">
-        Creating an account using the address {email}
-      </Typography.Paragraph>
-      <form
-        className="flex w-full flex-col"
-        onSubmit={(e) => onSubmit(e, submit)}
-      >
-        <Input
-          onChange={(e) => changePassword(e.target.value)}
-          error={error}
-          value={password}
-          label="Password"
-          className="mb-10"
-          autoFocus
-          ref={ref}
-          type={isPasswordShown ? "text" : "password"}
-          icon={
-            <TogglePasswordButton
-              isPasswordVisible={isPasswordShown}
-              togglePasswordVisibility={togglePasswortView}
-            />
-          }
-        />
-        <span>
-          <Button
-            className={`${!password && "pointer-events-none bg-[#1f4964]"}`}
-            disabled={!password}
-            size={"m"}
-            intent={"filled"}
-          >
-            Resume
-          </Button>
-        </span>
-      </form>
-    </div>
+    <AuthTemplate
+      title="Registration"
+      subtitle={`Creating an account using the address ${email}`}
+      onArrowClick={goBack}
+      onSubmit={onSubmit}
+    >
+      <Input
+        onChange={(e) => changePassword(e.target.value)}
+        error={error}
+        value={password}
+        label="Password"
+        className="mb-10"
+        autoFocus
+        ref={ref}
+        type={isPasswordShown ? "text" : "password"}
+        icon={
+          <TogglePasswordButton
+            isPasswordVisible={isPasswordShown}
+            togglePasswordVisibility={togglePasswortView}
+          />
+        }
+      />
+      <span>
+        <Button
+          className={`${!password && "pointer-events-none bg-[#1f4964]"}`}
+          disabled={!password}
+          size={"m"}
+          intent={"filled"}
+        >
+          Resume
+        </Button>
+      </span>
+    </AuthTemplate>
   )
 }
--- client/src/features/authentication/verify-email/ui.tsx
+++ client/src/features/authentication/verify-email/ui.tsx
@@ -1,53 +1,34 @@
-import { useRef, FormEvent } from "react"
+import { useRef } from "react"
 import { useUnit } from "effector-react"
 
-import { Button } from "@/shared/ui/buttons/main-button"
-import { Typography } from "@/shared/ui/general/typography"
-import { Icon } from "@/shared/ui/icon"
 import { CodeInput } from "@/shared/ui/data-entry/code-input"
+import { AuthTemplate } from "@/shared/ui/templates/auth/ui"
 
 import { $code, codeChanged, submitTriggered } from "./verify.model"
 
 export const VerifyEmail = ({ goBack }: { goBack: () => void }) => {
-  const [code, changeCode, submit] = useUnit([
+  const ref = useRef<HTMLInputElement>(null)
+  const [code, changeCode, onSubmit] = useUnit([
     $code,
     codeChanged,
     submitTriggered
   ])
-  const onSubmit = (e: FormEvent) => {
-    e.preventDefault()
-    submit()
-  }
-  const ref = useRef<HTMLInputElement>(null)
   return (
-    <div className="relative text-center text-cFont">
-      <Button
-        className="absolute -top-1 -left-[20px]"
-        onClick={goBack}
-        intent={"primary"}
-        size={"sm"}
-      >
-        <Icon
-          name="common/arrow"
-          className="text-4 rotate-180 text-cIconDefault"
-        />
-      </Button>
-      <Typography.Heading size="base" className="mb-3 font-semibold">
-        Verification
-      </Typography.Heading>
-      <form
-        className="flex justify-center"
-        onSubmit={(e) => onSubmit(e)}
-      >
-        <CodeInput 
-          inputStyle="focus:border-cHover bg-transparent border-b p-1 mx-0 w-6 border-cSecondBorder"
-          containerStyle="gap-2 py-5"
-          length={6} 
-          value={code} 
-          onChange={(e) => changeCode(e)} 
-          ref={ref}/>
-      </form>
-    </div>
+    <AuthTemplate
+      title="Verification"
+      subtitle="Enter the verification code to authenticate your account"
+      onArrowClick={goBack}
+      onSubmit={onSubmit}
+      className="flex justify-center"
+    >
+    <CodeInput 
+      inputStyle="focus:border-cHover bg-transparent border-b p-1 mx-0 w-6 border-cSecondBorder"
+      containerStyle="gap-2 py-5"
+      length={6} 
+      value={code} 
+      onChange={(e) => changeCode(e)} 
+      ref={ref}/>
+    </AuthTemplate>
   )
 }
 
