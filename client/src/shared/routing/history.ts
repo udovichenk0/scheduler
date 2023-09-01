c9719f9152b348483bfc7231c1d298d59d1c5845
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2023-09-02 00:04:35 +0300

    fix(removed redundant history that caused test errors)

--- client/src/shared/routing/history.ts
+++ client/src/shared/routing/history.ts
@@ -2,7 +2,6 @@ import { createEvent, sample } from "effector"
 import { createBrowserHistory } from "history"
 
 import { router } from "./router"
-export const history = createBrowserHistory()
 export const createHistory = () => {
   const init = createEvent()
   const history = createBrowserHistory()
