dd5b34220c4babde4e7a56ff390aa2bf9e0c5b31
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2023-09-08 16:44:30 +0300

    fix(fix variable in test mode)

--- client/src/shared/api/auth-query/base-query.test.ts
+++ client/src/shared/api/auth-query/base-query.test.ts
@@ -2,12 +2,13 @@ import { describe, expect, test } from "vitest"
 
 import { setQuery, setUrlParams } from "./base-query"
 
+
+
 describe("base-query test", () => {
   const baseUrl = 'http://localhost:3000/'
   test("add params to url", () => {
     const urlAsString = "user"
     const urlAsFuncWithoutParams = () => `user`
-
     const stringWithoutParams = setUrlParams(urlAsString)
     const funcWithoutParams = setUrlParams(urlAsFuncWithoutParams)
     const funcWithParams = setUrlParams(({ id }) => `user/${id}`, { id: "1" })
