3ae988db9e21b809738354df1c519df01dabb0b1
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2023-10-18 22:24:46 +0300

    fix(open date-picked with a task's date, not a current date)

--- client/src/shared/lib/date/generate-calendar.ts
+++ client/src/shared/lib/date/generate-calendar.ts
@@ -1,13 +1,15 @@
 import dayjs from "dayjs"
 
-export const generateCalendar = (currentMonth = dayjs().month()) => {
-  const year = dayjs().year()
-  const firstDayOfMonth = dayjs(new Date(year, currentMonth)).day()
+export const generateCalendar = ({
+  year = dayjs().year(), 
+  month = dayjs().month()
+}) => {
+  const firstDayOfMonth = dayjs(new Date(year, month)).day()
   let current = 0 - firstDayOfMonth
   return Array.from({ length: 5 }, () => []).map(() => {
     return Array.from({ length: 7 }, () => null).map(() => {
       current += 1
-      const currentDate = dayjs(new Date(year, currentMonth, current))
+      const currentDate = dayjs(new Date(year, month, current))
       return {
         date: currentDate.date(),
         month: currentDate.month(),
--- client/src/shared/ui/date-picker/ui/month-swithcher.tsx
+++ client/src/shared/ui/date-picker/ui/month-swithcher.tsx
@@ -1,4 +1,4 @@
-import dayjs from "dayjs"
+import dayjs, { Dayjs } from "dayjs"
 import { useTranslation } from "react-i18next"
 
 import { Button } from "../../buttons/main-button"
@@ -6,31 +6,31 @@ import { Icon } from "../../icon"
 
 export const MonthSwitcher = ({
   changeMonth,
-  displayedMonth,
+  date,
 }: {
-  changeMonth: (month: number) => void
-  displayedMonth: number
+  changeMonth: (date: Dayjs) => void
+  date: Dayjs
 }) => {
   const { t } = useTranslation()
-  const isCurrentMonth = dayjs().month() === displayedMonth
+  const isCurrentDate = dayjs().isSame(date, "month") && dayjs().isSame(date, "year")
   return (
     <div className="flex items-center justify-end gap-2">
       <Button
-        disabled={isCurrentMonth}
+        disabled={isCurrentDate}
         intent={"primary"}
         className="h-6 w-6"
-        onClick={() => changeMonth(displayedMonth - 1)}
+        onClick={() => changeMonth(date.add(-1, 'month'))}
       >
         <Icon
           name="common/arrow"
-          className={`-rotate-90 text-[10px] ${isCurrentMonth && "opacity-50"}`}
+          className={`-rotate-90 text-[10px] ${isCurrentDate && "opacity-50"}`}
         />
       </Button>
       <button
-        onClick={() => changeMonth(dayjs().month())}
-        disabled={isCurrentMonth}
+        onClick={() => changeMonth(dayjs())}
+        disabled={isCurrentDate}
         className={`text-[11px] font-bold text-accent ${
-          isCurrentMonth && "opacity-80"
+          isCurrentDate && "opacity-80"
         }`}
       >
         {t("calendar.today")}
@@ -38,7 +38,7 @@ export const MonthSwitcher = ({
       <Button
         intent={"primary"}
         className="h-6 w-6"
-        onClick={() => changeMonth(displayedMonth + 1)}
+        onClick={() => changeMonth(date.add(1, 'month'))}
       >
         <Icon name="common/arrow" className="rotate-90 text-[10px]" />
       </Button>
