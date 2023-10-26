be3341b87beb40960a6bfec04d5f5ccec535386c
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2023-10-26 17:38:36 +0300

    fix(updated urls)

--- client/src/shared/api/token/token.api.ts
+++ client/src/shared/api/token/token.api.ts
@@ -8,7 +8,7 @@ import { setTokenTriggered } from "./token.model"
 const RefreshContract = zodContract(TokenSchema)
 
 export const refreshFx = createEffect<void, TokenDto>(async () => {
-  const data = await fetch("http://localhost:3000/refresh", {
+  const data = await fetch(import.meta.env.VITE_ORIGIN_URL + "/refresh", {
     method: "GET",
     credentials: "include",
   })
