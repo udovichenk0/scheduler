b8cb1d2be21a8b812ecebfd6397241fb8f83c0f7
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2023-10-26 18:37:46 +0300

    test controller

--- server/src/domain/token/token.controller.ts
+++ server/src/domain/token/token.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Get, Header, Req, Session } from '@nestjs/common';
+import { Controller, Get, Req, Session } from '@nestjs/common';
 import { TokenService } from './token.service';
 import { Request } from 'express';
 
 @Controller()
 export class TokenController {
   constructor(private tokenService: TokenService) {}
-  @Header('Access-Control-Allow-Credentials', 'true')
-  @Header('Access-Control-Allow-Origin', '*')
   @Get('refresh')
   async refresh(@Req() req: Request, @Session() session: Record<string, any>) {
+    const refreshToken = req.session['refresh_token'];
+    return await this.tokenService.refresh(refreshToken, session);
+  }
+  @Get('')
+  async test(@Req() req: Request, @Session() session: Record<string, any>) {
     const refreshToken = req.session['refresh_token'];
     return await this.tokenService.refresh(refreshToken, session);
   }
