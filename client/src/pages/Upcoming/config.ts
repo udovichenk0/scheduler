b0b2310ac65be0e3f4e3bb28649dafef07b099f9
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2023-12-01 17:42:07 +0200

    feature(add the opportunity to sort by time)

--- client/src/pages/Upcoming/config.ts
+++ client/src/pages/Upcoming/config.ts
@@ -12,6 +12,14 @@ export const FILTER_CONFIG = [
     value: "alph_asc" as const,
     label: "By alphabet(asc)",
   },
+  {
+    value: "time_asc" as const,
+    label: "By time(asc)",
+  },
+  {
+    value: "time_desc" as const,
+    label: "By time(desc)",
+  },
 ]
 
 export function generateSequentialDates() {
