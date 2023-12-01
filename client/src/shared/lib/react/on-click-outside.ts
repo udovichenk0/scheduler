1bac6e1d4702c8117e9cb0a4ab08f897aeddd40d
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2023-12-01 15:26:23 +0200

    refactor(move out code to hook useClickOutside)

--- client/src/shared/lib/react/on-click-outside.ts
+++ client/src/shared/lib/react/on-click-outside.ts
@@ -1,4 +1,4 @@
-import { RefObject, MouseEvent } from "react"
+import { RefObject, useEffect } from "react"
 
 export const onClickOutside = (
   outRef: RefObject<HTMLElement>,
@@ -8,4 +8,24 @@ export const onClickOutside = (
   if (outRef.current && !outRef.current.contains(e.target as Node)) {
     callback()
   }
+}
+
+type UseClickOutsideProps = {
+  callback: () => void
+  deps: unknown[]
+  ref: RefObject<HTMLElement>
+}
+export const useClickOutside = (props: UseClickOutsideProps) => {
+  const { callback, deps, ref } = props
+  useEffect(() => {
+    const closeFilter = (e: MouseEvent) => {
+      if (!(ref.current)?.contains(e.target as Node)) {
+        callback()
+      }
+    }
+      document.addEventListener("click", closeFilter)
+    return () => {
+      document.removeEventListener("click", closeFilter)
+    }
+  }, deps)
 }
