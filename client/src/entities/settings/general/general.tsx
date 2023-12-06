78fa121ec3c2715440589380fb5bf19b2497490f
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2023-12-06 18:01:01 +0200

    fix(scope was lost)

--- client/src/entities/settings/general/general.tsx
+++ client/src/entities/settings/general/general.tsx
@@ -1,10 +1,16 @@
 import { useTranslation } from "react-i18next"
+import { useEffect } from "react"
+import { useUnit } from 'effector-react'
 
 import { languages } from "./config"
-import { $currentLanguage, languageChanged } from "./general.modal"
+import { $currentLanguage, init, languageChanged } from "./general.modal"
 import { Selector } from "./ui/selector"
 export const GeneralSettings = () => {
+  const onInit = useUnit(init)
   const { t } = useTranslation()
+  useEffect(() => {
+    onInit()
+  }, [])
   return (
     <div className="mx-auto mb-6 w-[90%] space-y-3">
       <div className="flex items-center justify-end space-x-1">
