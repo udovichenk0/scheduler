a7af61e6d0ba8fadb3e355104dc9e911107b801f
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2023-12-09 16:41:33 +0200

    fix(using naviagte didn't change the page view, made versatile button)

--- client/src/shared/ui/buttons/main-button/ui.tsx
+++ client/src/shared/ui/buttons/main-button/ui.tsx
@@ -1,20 +1,38 @@
 import { VariantProps } from "class-variance-authority"
 import { clsx } from "clsx"
 import { ButtonHTMLAttributes, ReactNode } from "react"
+import { RouteInstance, RouteParams, RouteQuery } from "atomic-router"
+import { Link } from "atomic-router-react"
 
 import { buttonCva } from "./cva.styles"
-
-export interface ButtonProps
-  extends ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonCva> {
-  children?: ReactNode
+type BaseProps = VariantProps<typeof buttonCva> & {
+  children: ReactNode
 }
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
+  BaseProps & {
+    as?: "button"
+  }
+type LinkProps = 
+  BaseProps & {
+    as: "link"
+    to: string | RouteInstance<RouteParams>;
+    params?: RouteParams | undefined;
+    query?: RouteQuery | undefined;
+    activeClassName?: string | undefined;
+    inactiveClassName?: string | undefined;
+    ref?: React.ForwardedRef<HTMLAnchorElement> | undefined;
+  } & React.AnchorHTMLAttributes<HTMLAnchorElement>
 
-export const Button = ({ intent, size, children, ...props }: ButtonProps) => {
-  const { className, ...rest } = props
+type ButtonOrLinkProps = LinkProps | ButtonProps
+export const Button = ({ intent, size, children, className, ...props }: ButtonOrLinkProps) => {
+  if(props.as == 'link'){
+    return (
+      <Link {...props} to={props.to} className={clsx(className, buttonCva({size, intent}))}>{children}</Link>
+    )
+  }
   return (
-    <button className={clsx(buttonCva({ size, intent }), className)} {...rest}>
-      {children}
-    </button>
+      <button className={clsx(className, buttonCva({ size, intent }))} {...props}>
+        {children}
+      </button>
   )
 }
