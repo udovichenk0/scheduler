2b2d514b762fb7063a649def6927d04d57d43842
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2023-12-15 19:52:09 +0200

    feature(preferred theme color, refactor)

--- client/src/app/initializer.ts
+++ client/src/app/initializer.ts
@@ -1,17 +1,18 @@
 import { createEvent, sample } from "effector"
 
+import { $$themeSettings } from "@/entities/settings/theme/main-theme"
+import { $$accentSettings } from "@/entities/settings/theme/icon-theme"
+
 import { createHistory } from "@/shared/routing"
-import { createTheme } from "@/shared/theme/create-theme"
 import { $$i18n } from "@/shared/i18n"
 
 export const appInitializer = () => {
   const init = createEvent()
-  const $$themeInitializer = createTheme()
   const $$routerHistory = createHistory()
-
+  const theme = $$themeSettings
   sample({
     clock: init,
-    target: [$$themeInitializer.init, $$routerHistory.init, $$i18n.init],
+    target: [$$accentSettings.init, $$routerHistory.init, $$i18n.init, theme.init],
   })
   return {
     init,
