ad7ae63c610391700603417732cb092742d1357b
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2023-12-26 21:41:16 +0200

    refactor(move some logic from controller to service)

--- server/src/domain/auth/auth.service.ts
+++ server/src/domain/auth/auth.service.ts
@@ -6,39 +6,31 @@ import {
 } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma.service';
 import { AuthCredentialsDto } from './dto/auth.dto';
-import { encryptPassword } from 'src/lib/hash-password/encrypt';
 import { TokenService } from '../token/token.service';
 import { UserDto } from '../user/dto/user.dto';
 import { passwordNotCorrect, userNotFound } from './constant/authErrorMessages';
 import { compareHash } from 'src/lib/hash-password/compareHash';
 import { v4 as uuidv4 } from 'uuid';
 import { sendEmail } from 'src/nodemailer/send-email';
+import { UserService } from '../user/user.service';
 import { randomCode } from 'src/lib/random-code';
 @Injectable()
 export class AuthService {
   constructor(
     private prismaService: PrismaService,
     private tokenService: TokenService,
+    private userService: UserService,
   ) {}
 
-  async createUser(credentials: AuthCredentialsDto) {
-    const potentialUser = await this.prismaService.user.findUnique({
-      where: {
-        email: credentials.email,
-      },
+  async createUser(data: AuthCredentialsDto) {
+    const potentialUser = await this.userService.findOne({
+      email: data.email,
     });
     if (potentialUser) {
       throw new ConflictException('User already exist');
     }
-    const hash = await encryptPassword(credentials.password);
     const code = randomCode();
-    const user = await this.prismaService.user.create({
-      data: {
-        email: credentials.email,
-        hash,
-        id: uuidv4(),
-      },
-    });
+    const user = await this.userService.createOne(data);
     await this.prismaService.emailConfirmation.create({
       data: {
         id: uuidv4(),
@@ -46,36 +38,33 @@ export class AuthService {
         user_id: user.id,
       },
     });
-    await sendEmail(credentials.email, code);
+    await sendEmail(data.email, code);
     return user;
   }
-  async verifyUser(credentials: AuthCredentialsDto) {
-    const user = await this.prismaService.user.findUnique({
-      where: {
-        email: credentials.email,
-      },
+  async verifyUser({ email, password }: AuthCredentialsDto) {
+    const user = await this.userService.findOne({
+      email,
     });
     if (!user) {
-      throw new NotFoundException(userNotFound(credentials.email));
+      throw new NotFoundException(userNotFound(email));
     }
-    const compared = await compareHash(user.hash, credentials.password);
+    const compared = await compareHash(user.hash, password);
     if (!compared) {
       throw new NotFoundException(passwordNotCorrect);
     }
+    const userDto = UserDto.create(user);
     const { access_token, refresh_token } = await this.tokenService.issueTokens(
-      UserDto.create(user),
+      UserDto.create(userDto),
     );
     return {
-      user,
+      user: userDto,
       access_token,
       refresh_token,
     };
   }
   async verifyEmail({ code, email }: { code: string; email: string }) {
-    const user = await this.prismaService.user.findUnique({
-      where: {
-        email,
-      },
+    const user = await this.userService.findOne({
+      email,
     });
     if (!user) {
       throw new NotFoundException(userNotFound(email));
@@ -92,24 +81,18 @@ export class AuthService {
     if (!isValid) {
       throw new NotAcceptableException('Code is not valid');
     }
-    const verifiedUser = await this.prismaService.user.update({
-      where: {
-        id: user.id,
-      },
-      data: {
-        verified: true,
-      },
-    });
+    const verifiedUser = await this.userService.verify(user.id);
     await this.prismaService.emailConfirmation.delete({
       where: {
         user_id: verifiedUser.id,
       },
     });
+    const userDto = UserDto.create(verifiedUser);
     const { access_token, refresh_token } = await this.tokenService.issueTokens(
-      UserDto.create(user),
+      userDto,
     );
     return {
-      user: verifiedUser,
+      user: userDto,
       access_token,
       refresh_token,
     };
