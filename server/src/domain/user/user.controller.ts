1e0dbeade0567bee4a9fd5b4d08cee868481f7e9
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2023-12-30 13:55:57 +0200

    refactor(separated method for handling verified user)

--- server/src/domain/user/user.controller.ts
+++ server/src/domain/user/user.controller.ts
@@ -1,13 +1,12 @@
 import { Controller, Get, Query, UsePipes } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserCredentials, UserDto } from './dto/user.dto';
+import { UserCredentials } from './dto/user.dto';
 @Controller()
 export class UserController {
   constructor(private userService: UserService) {}
   @Get('user')
   @UsePipes(UserCredentials)
   async getUser(@Query('email') email: string) {
-    const user = await this.userService.findOne({ email });
-    return UserDto.create(user);
+    return await this.userService.findVerifiedUser({ email });
   }
 }
