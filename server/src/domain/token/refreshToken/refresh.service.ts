630ebdc10b0ed3b8c6d24a452939651a9a0f9d2e
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2023-12-30 22:00:25 +0200

    remove dead code

--- server/src/domain/token/refreshToken/refresh.service.ts
+++ server/src/domain/token/refreshToken/refresh.service.ts
@@ -1,11 +1,5 @@
-import {
-  Injectable,
-  NotFoundException,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { sign, verify } from 'jsonwebtoken';
-import { PrismaService } from 'src/database/prisma.service';
-import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
 import { UserDto } from 'src/domain/user/dto/user.dto';
 import {
   refreshExpiredMessage,
@@ -17,7 +11,6 @@ import {
 } from './constant/refreshErrors';
 @Injectable()
 export class RefreshService {
-  constructor(private prismaService: PrismaService) {}
   async signRefresh(userData: UserDto): Promise<string> {
     const token = await sign(userData, process.env.JWT_SECRET, {
       expiresIn: '15d',
