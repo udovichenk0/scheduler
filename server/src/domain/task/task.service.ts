c77ef91c94856c8c30c469bfb56bdb7c83220422
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2023-12-31 10:39:56 +0200

    fix(avoid redundant db requests)

--- server/src/domain/task/task.service.ts
+++ server/src/domain/task/task.service.ts
@@ -44,13 +44,16 @@ export class TaskService {
     user_id: string;
     data: Omit<Prisma.taskCreateManyInput, 'user_id' | 'id'>[];
   }) {
+    if (!data.length) {
+      return [];
+    }
     const result = await this.prismaService.task.createMany({
       data: data.map((task) => ({
         ...task,
         user_id,
       })),
     });
-    if (result) {
+    if (result.count) {
       return await this.findMany({ id: user_id });
     }
     return null;
