5e7126490ce4c5394227aa21b5263af414bc3a2c
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2024-01-02 22:50:40 +0200

    temporarily

--- client/src/features/authentication/check-email/ui.tsx
+++ client/src/features/authentication/check-email/ui.tsx
@@ -16,13 +16,20 @@ export const CheckEmailForm = ({ goBack }: { goBack: () => void }) => {
   const error = useUnit($emailError)
   const changeEmail = useUnit(emailChanged)
   const onSubmit = useUnit(submitTriggered)
-
+  const submit = () => {
+    if(import.meta.env.MODE == "development"){
+      onSubmit()
+    }
+    else {
+      alert('Temporarily not available, but the functionality is the same')
+    }
+  }
   return (
     <AuthTemplate
       title={t("setting.synchronization.byEmail.title")}
       subtitle={t("setting.synchronization.main.description")}
       onArrowClick={goBack}
-      onSubmit={onSubmit}
+      onSubmit={submit}
     >
       <Input
         onChange={(e) => changeEmail(e.target.value)}
