e062e0f51e2dc994be0c6442465926cbdcb43a28
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2024-01-18 21:13:39 +0200

    virtualized list for date picker

--- client/src/shared/ui/date-picker/ui/week-names.tsx
+++ client/src/shared/ui/date-picker/ui/week-names.tsx
@@ -5,7 +5,7 @@ import { SHORT_WEEKS_NAMES } from "@/shared/config/constants"
 export const WeeksName = () => {
   const { t } = useTranslation()
   return (
-    <div className="flex justify-around border-b-[1px] border-cBorder text-primary">
+    <div className="flex justify-around text-primary">
       {SHORT_WEEKS_NAMES.map((name) => {
         return (
           <span className="justify-self-center py-2 text-[12px]" key={name}>
