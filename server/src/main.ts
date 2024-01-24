e7b949a65fec93adbf97bc0fe017276c030bf906
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2024-01-24 13:58:02 +0200

    refactor

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,47 +2,32 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
 import { ConfigService } from '@nestjs/config';
-import { job } from 'cron';
-import { PrismaClient } from '@prisma/client';
-const prismaClient = new PrismaClient();
-job(
-  '0 */24 * * *',
-  async function () {
-    await prismaClient.user.deleteMany({
-      where: {
-        AND: [
-          { verified: false },
-          {
-            created_at: {
-              lte: new Date(new Date().getTime() - 24 * 60 * 60 * 1000),
-            },
-          },
-        ],
-      },
-    });
-  },
-  null,
-  true,
-);
+import { UserService } from './domain/user/user.service';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  app.enableCors({
-    origin: configService.get('CLIENT_URL'),
-    credentials: true,
-  });
-  app.use(
-    session({
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      //@ts-ignore
-      secret: configService.get('SESSION_SECRET'),
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        httpOnly: true,
-      },
-    }),
-  );
-  await app.listen(3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
+    app.enableCors({
+      origin: configService.get('CLIENT_URL'),
+      credentials: true,
+    });
+    app.use(
+      session({
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //@ts-ignore
+        secret: configService.get('SESSION_SECRET'),
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+          httpOnly: true,
+        },
+      }),
+    );
+    UserService.userCollector()
+    await app.listen(3000);
+  } catch (error) {
+
+  }
 }
 bootstrap();
