5f1ef8d1d9ac704bd882f9619c8dbfcacd03fdc3
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2024-01-28 17:54:39 +0200

    fix(date picker)

---
+++ client/src/shared/ui/date-picker/ui/footer.tsx
@@ -0,0 +1,22 @@
+import { useTranslation } from "react-i18next"
+import { Button } from "../../buttons/main-button"
+
+export const Footer = ({
+  onCancel,
+  onSave
+}: {
+  onCancel: () => void,
+  onSave: () => void
+}) => {
+  const { t } = useTranslation()
+  return (
+    <div className="flex gap-3 text-primary">
+      <Button onClick={onCancel} className="w-full p-[1px] text-[12px]">
+        {t("calendar.cancel")}
+      </Button>
+      <Button onClick={onSave} intent={'filled'} className="w-full p-[1px] text-[12px]">
+        {t("calendar.ok")}
+      </Button>
+    </div>
+  )
+}
